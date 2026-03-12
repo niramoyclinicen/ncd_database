1bf246f5f0f560e8cadb22ed7cee27b2e730bd98
niramoyclinicen <niramoyclinicen@gmail.com>
2026-03-12 19:40:14 +0600

    Add files via upload

--- components/PrevDueCollectionPage.tsx
+++ components/PrevDueCollectionPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { LabInvoice, Employee, DueCollection } from './DiagnosticData';
 import SearchableSelect from './SearchableSelect';
 import { formatDateTime } from '../utils/dateUtils';
